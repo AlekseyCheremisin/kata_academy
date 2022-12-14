0185b68bcf93cf7c3711914f60417b5e1607357c
AlekseyCheremisin <aleksey.cher3misin@yandex.ru>
2022-12-14 18:04:50 +0300

    Add Scanner for reading from console

--- src/academy/kata/Main.java
+++ src/academy/kata/Main.java
@@ -1,5 +1,8 @@
 package academy.kata;
 
+import java.util.Scanner;
+
+
 public class Main {
     public static String calc(String input) throws
             OutOfRangeException,
@@ -21,25 +24,8 @@ public class Main {
     }
 
     public static void main(String[] args) throws OutOfRangeException, NotMathOperation, OperationNotFoundException, InvalidRomanNumFormatException, NegativeRomanResultException {
-        String[] operations = new String[]{" + ", " - ", " / ", " * "};
-        StringBuilder sb = new StringBuilder();
-        for (String operation : operations) {
-            for (int i = 1; i < 11; i++) {
-                for (int j = 1; j < 11; j++) {
-                    RomanNum first = new RomanNum(i);
-                    RomanNum second = new RomanNum(j);
-                    sb.append(first.getRomanNumStr());
-                    sb.append(operation);
-                    sb.append(second.getRomanNumStr());
-                    String numStr = first.getRomanNumInt() + operation + second.getRomanNumInt();
-                    try {
-                        System.out.println(sb + " = " + calc(sb.toString()) + " | " + numStr + " = " + calc(numStr));
-                    } catch (NegativeRomanResultException e) {
-                        System.out.println(sb + " = " + e.getMessage() + " | " + numStr + " = " + calc(numStr));
-                    }
-                    sb.delete(0, sb.length());
-                }
-            }
-        }
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        System.out.println(calc(str));
     }
 }
